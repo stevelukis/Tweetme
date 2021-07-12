f1fd283f33bf6151855bac620564d82cdd962784
Steve Lukis <stevelukis88@gmail.com>
2021-07-12 15:08:16 +0800

    Added new container for tweets

--- tweetme2-web/src/index.js
+++ tweetme2-web/src/index.js
@@ -2,14 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {TweetComponent} from "./tweets";
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const appEl = document.getElementById('root')
+
+if (appEl) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>,
+    appEl
+  );
+}
+
+const tweetsEl = document.getElementById('tweetme-2')
+
+if (tweetsEl) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <TweetComponent />
+    </React.StrictMode>,
+    tweetsEl
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
